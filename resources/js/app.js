e58d9a303a6ab7c76d763578432f0689476edcab
AdinaVoicu <adina@managementit.ro>
2022-11-08 21:28:30 +0200

    Add fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,15 @@
 import './bootstrap'
 import { createApp } from 'vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser)
+
 import MoviesIndex from './components/App.vue'
 
 const app = createApp({})
 app.component('moviesindex', MoviesIndex)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
