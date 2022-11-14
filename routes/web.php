ab10b38e913cfe512ce4729c691168e4b844bd12
AdinaVoicu <adina@managementit.ro>
2022-11-14 16:16:50 +0200

    Routing Fix - for all Vue.js Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('movies.index');
-});
+Route::view('/{any?}', 'movies.index')
+    ->name('dashboard')
+    ->where('any', '.*');
