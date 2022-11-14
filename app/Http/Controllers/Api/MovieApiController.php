d569811431e8396aae7a231d16df83c5719cb0d0
AdinaVoicu <adina@managementit.ro>
2022-11-14 17:40:24 +0200

    Rest API: post, delete

--- app/Http/Controllers/Api/MovieApiController.php
+++ app/Http/Controllers/Api/MovieApiController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMovieRequest;
 use App\Http\Resources\MovieResource;
-use App\Models\Artist;
 use App\Models\Movie;
 use Illuminate\Auth\Access\Gate;
-use Illuminate\Support\Facades\DB;
 
 // use MovieResource;
 
@@ -23,5 +22,17 @@ class MovieApiController extends Controller
 	public function show(Movie $movie)
 	{
 		return new MovieResource($movie);
+	}
+	public function store(StoreMovieRequest $request)
+	{
+		$movie = Movie::create($request->validated());
+
+		return new MovieResource($movie);
+	}
+	public function destroy(Movie $movie)
+	{
+		$movie->delete();
+
+		return "Deleted";
 	}
 }
